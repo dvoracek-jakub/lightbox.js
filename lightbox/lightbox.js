9f087846b740e7f232c39c59f4bda5810574660e
Jakub Dvořáček <45931076+dvoracek-jakub@users.noreply.github.com>
2020-04-03 10:14:14 +0200

    Update lightbox.js

--- lightbox/lightbox.js
+++ lightbox/lightbox.js
@@ -4,249 +4,185 @@
  * Author: Victor Diego <victordieggo@gmail.com>
  * License: MIT
  * ================================================================== */
+$ = jQuery;
+$(document).ready(function () {
+    (function () {
 
-const lightbox = (element) => {
-  'use strict';
+        'use strict';
 
-  let btnClose;
-  let btnNav;
-  let currentItem;
-  let container;
-  let content;
-  let wrapper;
+        var animation, body, btnClose, btnNav, currentItem, container, content, wrapper, trigger, currentTrigger;
 
-  const doc = document;
-  const body = doc.body;
+        body = document.body;
 
-  const animation = {
-    fadeIn: 'fadeIn .3s',
-    fadeOut: 'fadeOut .3s',
-    scaleIn: 'scaleIn .3s',
-    scaleOut: 'scaleOut .3s'
-  };
+        trigger = body.querySelectorAll('[data-lightbox]');
 
-  const classes = {
-    btn: 'lightbox-btn',
-    gallery: 'lightbox-gallery',
-    firstTriggered: 'lightbox-trigger',
-    currentElement: 'lightbox-active-item'
-  };
+        animation = {
+            fadeIn: 'fadeIn .3s',
+            fadeOut: 'fadeOut .3s',
+            scaleIn: 'createBox .3s',
+            scaleOut: 'deleteBox .3s'
+        };
 
-  const toggleScroll = () => body.classList.toggle('remove-scroll');
-
-  const sortContent = (content) => {
-
-    if (element.tagName === 'A') {
-      content = content.getAttribute('href');
-    }
-
-    if (content.match(/\.(jpeg|jpg|gif|png)/)) {
-      const image = doc.createElement('img');
-      image.className = 'lightbox-image';
-      image.src = content;
-      if (element.tagName === 'A') {
-        image.alt = element.getAttribute('data-image-alt');
-      }
-      return image;
-    }
-
-    if (content.match(/(youtube|vimeo)/)) {
-      const video = [];
-
-      if (content.match('youtube')) {
-        video.id = content.split(/v\/|v=|youtu\.be\//)[1].split(/[?&]/)[0];
-        video.url = 'youtube.com/embed/';
-        video.options = '?autoplay=1&rel=0';
-      }
-
-      if (content.match('vimeo')) {
-        video.id = content.split(/video\/|https:\/\/vimeo\.com\//)[1].split(/[?&]/)[0];
-        video.url = 'player.vimeo.com/video/';
-        video.options = '?autoplay=1title=0&byline=0&portrait=0';
-      }
-
-      video.player = doc.createElement('iframe');
-      video.player.setAttribute('allowfullscreen', '');
-      video.player.className = 'lightbox-video-player';
-      video.player.src = 'https://' + video.url + video.id + video.options;
-
-      video.wrapper = doc.createElement('div');
-      video.wrapper.className = 'lightbox-video-wrapper';
-      video.wrapper.appendChild(video.player);
-
-      return video.wrapper;
-    }
-
-    return doc.querySelector(content).children[0].cloneNode(true);
-  };
-
-  const galleryItens = (element) => {
-    const itens = {
-      next: element.parentElement.nextElementSibling,
-      previous: element.parentElement.previousElementSibling
-    };
-    for (let key in itens) {
-      if (itens[key] !== null) {
-        itens[key] = itens[key].querySelector('[data-lightbox]');
-      }
-    }
-    return itens;
-  };
-
-  const build = () => {
-    btnClose = doc.createElement('button');
-    btnClose.setAttribute('aria-label', 'Close');
-    btnClose.className = classes.btn + ' ' + classes.btn + '-close';
-
-    content = doc.createElement('div');
-    content.className = 'lightbox-content';
-    content.appendChild(sortContent(element));
-
-    wrapper = content.cloneNode(false);
-    wrapper.className = 'lightbox-wrapper';
-    wrapper.style.animation = [animation.scaleIn, animation.fadeIn];
-    wrapper.appendChild(btnClose);
-    wrapper.appendChild(content);
-
-    container = content.cloneNode(false);
-    container.className = 'lightbox-container';
-    container.style.animation = animation.fadeIn;
-    container.onclick = () => {};
-    container.appendChild(wrapper);
-
-    if (element.tagName === 'A' && element.getAttribute('data-lightbox') === 'gallery') {
-      container.classList.add(classes.gallery);
-      btnNav = {previous: '', next: ''};
-      for (let key in btnNav) {
-        if (btnNav.hasOwnProperty(key)) {
-          btnNav[key] = btnClose.cloneNode(false);
-          btnNav[key].setAttribute('aria-label', key);
-          btnNav[key].className = classes.btn + ' ' + classes.btn + '-' + key;
-          btnNav[key].disabled = !galleryItens(element)[key] ? true : false;
-          wrapper.appendChild(btnNav[key]);
+        function toggleScroll() {
+            body.classList.toggle('remove-scroll');
         }
-      }
-    }
 
-    if (element.tagName === 'A') {
-      element.blur();
-      currentItem = element;
-      element.classList.add(classes.currentElement);
-      element.classList.add(classes.firstTriggered);
-    }
+        function sortContent(content) {
+            var image, video, href = content.getAttribute('href');
 
-    element.parentNode.insertBefore(container, element.nextSibling);
-    toggleScroll();
-  };
+            if (href.match(/\.(jpeg|jpg|gif|png)/)) {
+                image = document.createElement('img');
+                image.className = 'lightbox-image';
+                image.src = href;
+                image.alt = content.getAttribute('data-image-alt');
+                return image;
+            }
 
-  const navigate = (position) => {
-    wrapper.removeAttribute('style');
-    const item = galleryItens(currentItem)[position];
-    if (item !== null) {
-      content.style.animation = animation.fadeOut;
-      setTimeout(() => {
-        content.replaceChild(sortContent(item), content.children[0]);
-        content.style.animation = animation.fadeIn;
-      }, 200);
-      currentItem.classList.remove(classes.currentElement);
-      item.classList.add(classes.currentElement);
-      currentItem = item;
-      for (let key in btnNav) {
-        if (btnNav.hasOwnProperty(key)) {
-          btnNav[key].disabled = !galleryItens(item)[key] ? true : false;
+            if (href.match(/(youtube|vimeo)/)) {
+                video = [];
+                if (href.match('youtube')) {
+                    video.id = href.split(/v\/|v=|youtu\.be\//)[1].split(/[?&]/)[0];
+                    video.url = 'www.youtube.com/embed/';
+                    video.options = '?autoplay=1&rel=0';
+                }
+                if (href.match('vimeo')) {
+                    video.id = href.split(/video\/|https:\/\/vimeo\.com\//)[1].split(/[?&]/)[0];
+                    video.url = 'player.vimeo.com/video/';
+                    video.options = '?autoplay=1title=0&byline=0&portrait=0';
+                }
+                video.player = document.createElement('iframe');
+                video.player.setAttribute('allowfullscreen', '');
+                video.player.className = 'lightbox-video-player';
+                video.player.src = 'https://' + video.url + video.id + video.options;
+                video.wrapper = document.createElement('div');
+                video.wrapper.className = 'lightbox-video-wrapper';
+                video.wrapper.appendChild(video.player);
+                return video.wrapper;
+            }
+
+            return body.querySelector(href).children[0].cloneNode(true);
         }
-      }
-    }
-  };
 
-  const close = () => {
-    toggleEvents('remove');
-    container.style.animation = animation.fadeOut;
-    wrapper.style.animation = [animation.scaleOut, animation.fadeOut];
-    setTimeout(() => {
-      toggleScroll();
-      element.parentNode.removeChild(container);
-      if (element.tagName === 'A') {
-        currentItem.classList.remove(classes.currentElement);
-        const trigger = doc.querySelector('.' + classes.firstTriggered);
-        trigger.classList.remove(classes.firstTriggered);
-        trigger.focus();
-      }
-    }, 200);
-  };
-
-  const controls = (event) => {
-    const target = event.target;
-    const key = event.keyCode;
-    const type = event.type;
-
-    const clickClose = type == 'click' && [container, btnClose].indexOf(target) !== -1;
-    const keyupClose = type == 'keyup' && key == 27;
-    if (clickClose || keyupClose) {
-      if (container.parentElement === element.parentElement) {
-        close();
-      }
-    }
-
-    if (btnNav) {
-      const clickNext = type == 'click' && target == btnNav.next;
-      const keyupNext = type == 'keyup' && key == 39;
-      if (clickNext || keyupNext) {
-        navigate('next');
-      }
-
-      const clickPrev = type == 'click' && target == btnNav.previous;
-      const keyupPrev = type == 'keyup' && key == 37;
-      if (clickPrev || keyupPrev) {
-        navigate('previous');
-      }
-    }
-
-    if (type == 'keydown' && key == 9) {
-      let focusable = ['[href]', 'button', 'input', 'select', 'textarea'];
-      focusable = focusable.map(i => i + ':not([disabled])');
-      focusable = container.querySelectorAll(focusable.toString());
-      const firstFocusable = focusable[0];
-      const lastFocusable = focusable[focusable.length - 1];
-
-      if (event.shiftKey) {
-        if (doc.activeElement == firstFocusable) {
-          lastFocusable.focus();
-          event.preventDefault();
+        function galleryItens(element) {
+            var itens = {
+                    next: element.parentElement.nextElementSibling,
+                    previous: element.parentElement.previousElementSibling
+                },
+                key;
+            for (key in itens) {
+                if (itens[key] !== null) {
+                    itens[key] = itens[key].querySelector('[data-lightbox]');
+                }
+            }
+            return itens;
         }
-      } else {
-        if (doc.activeElement == lastFocusable) {
-          firstFocusable.focus();
-          event.preventDefault();
+
+        function buildLightbox(element) {
+            element.blur();
+            currentItem = element;
+            element.classList.add('current-lightbox-item');
+
+            btnClose = document.createElement('button');
+            btnClose.className = 'lightbox-btn lightbox-btn-close';
+
+            content = document.createElement('div'); // < totok
+
+
+            content.className = 'lightbox-content';
+            content.appendChild(sortContent(element));
+
+            wrapper = content.cloneNode(false);
+            wrapper.className = 'lightbox-wrapper';
+            wrapper.style.animation = [animation.scaleIn, animation.fadeIn];
+            wrapper.appendChild(content);
+            wrapper.appendChild(btnClose);
+
+            container = content.cloneNode(false);
+            container.className = 'lightbox-container';
+            container.style.animation = animation.fadeIn;
+            container.onclick = function() {};
+            container.appendChild(wrapper);
+
+            if (element.getAttribute('data-lightbox') === 'gallery') {
+                container.classList.add('lightbox-gallery');
+                var key;
+                btnNav = {next: '', previous: ''};
+                for (key in btnNav) {
+                    if (btnNav.hasOwnProperty(key)) {
+                        btnNav[key] = btnClose.cloneNode(false);
+                        btnNav[key].className = 'lightbox-btn lightbox-btn-' + key;
+                        btnNav[key].disabled = galleryItens(element)[key] === null ? true : false;
+                        wrapper.appendChild(btnNav[key]);
+                    }
+                }
+            }
+
+            body.appendChild(container);
         }
-        lastFocusable.addEventListener('blur', () => {
-          if (lastFocusable.disabled) {
-            firstFocusable.focus();
-            event.preventDefault();
-          }
+
+        function galleryNavigation(position) {
+            wrapper.removeAttribute('style');
+            var item = galleryItens(currentItem)[position],
+                key;
+            if (item !== null) {
+                content.style.animation = animation.fadeOut;
+                setTimeout(function () {
+                    content.replaceChild(sortContent(item), content.children[0]);
+                    content.style.animation = animation.fadeIn;
+                }, 200);
+                currentItem.classList.remove('current-lightbox-item');
+                item.classList.add('current-lightbox-item');
+                currentItem = item;
+                for (key in btnNav) {
+                    if (btnNav.hasOwnProperty(key)) {
+                        btnNav[key].disabled = galleryItens(item)[key] === null ? true : false;
+                    }
+                }
+            }
+        }
+
+        function closeLightbox() {
+            container.style.animation = animation.fadeOut;
+            wrapper.style.animation = [animation.scaleOut, animation.fadeOut];
+            setTimeout(function () {
+                if (body.contains(container)) {
+                    body.removeChild(container);
+                    currentTrigger.focus();
+                    currentItem.classList.remove('current-lightbox-item');
+                    toggleScroll();
+                }
+            }, 200);
+        }
+
+        Array.prototype.forEach.call(trigger, function (element) {
+            element.addEventListener('click', function (event) {
+                event.preventDefault();
+                buildLightbox(element);
+                currentTrigger = element;
+            });
         });
-      }
-    }
-  };
 
-  const toggleEvents = (option) => {
-    ['click', 'keyup', 'keydown'].forEach((eventType) => {
-      if (option === 'remove') {
-        doc.removeEventListener(eventType, event => controls(event));
-        return;
-      }
-      doc.addEventListener(eventType, event => controls(event));
-    });
-  };
+        ['click', 'keyup'].forEach( function (eventType) {
+            body.addEventListener(eventType, function (event) {
+                if (body.contains(container)) {
+                    var target = event.target,
+                        key = event.keyCode,
+                        type = event.type;
+                    if ([container, btnClose].indexOf(target) !== -1 || key === 27) {
+                        closeLightbox();
+                    }
+                    if (container.classList.contains('lightbox-gallery')) {
+                        if ((target === btnNav.next && type === 'click') || key === 39) {
+                            galleryNavigation('next');
+                        }
+                        if ((target === btnNav.previous && type === 'click') || key === 37) {
+                            galleryNavigation('previous');
+                        }
+                    }
+                }
+            });
+        });
 
-  build();
-  toggleEvents();
-};
+    }());
 
-Array.prototype.forEach.call(document.querySelectorAll('[data-lightbox]'), (element) => {
-  'use strict';
-  element.addEventListener('click', (event) => {
-    event.preventDefault();
-    new lightbox(element);
-  });
-});
+})
